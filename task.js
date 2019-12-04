63a929faab8a263658112d24b90d9899f6e36ccc
hariaot <55063612+hariaot@users.noreply.github.com>
2019-12-03 22:08:34 -0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,4 @@
+let barraT = 0, barraP = 0, barraR = 0, barraC = 0, barra = 0;
+let buttom = document.getElementById("New").addEventListener("click", function(){
+	document.getElementById("Nova").style.display = "block";
+})
