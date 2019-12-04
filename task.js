7742fc5432829b3d7da416cbdbe28b87fcbe4ae4
hariaot <55063612+hariaot@users.noreply.github.com>
2019-12-03 22:33:25 -0300

    Add files via upload

--- task.js
+++ task.js
@@ -2,3 +2,18 @@ let barraT = 0, barraP = 0, barraR = 0, barraC = 0, barra = 0;
 let buttom = document.getElementById("New").addEventListener("click", function(){
 	document.getElementById("Nova").style.display = "block";
 })
+
+
+function calculaBarra(){
+    let tamanho = (qtToDo * 100)/qtTotal;
+    document.getElementById("BToDO").style.width = `${tamanho}%`;
+    
+    tamanho = (qtInProgress * 100)/qtTotal;
+    document.getElementById("BInPro").style.width = `${tamanho}%`;
+    
+    tamanho = (qtReview * 100)/qtTotal;
+    document.getElementById("BRevi").style.width = `${tamanho}%`;
+    
+    tamanho = (qtComplete * 100)/qtTotal;
+    document.getElementById("BComp").style.width = `${tamanho}%`;
+}
