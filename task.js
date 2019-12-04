60da55205b9e6450c54c1d55b980792b578ba008
hariaot <55063612+hariaot@users.noreply.github.com>
2019-12-04 16:31:39 -0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,11 +1,18 @@
-let barraT = 0, barraP = 0, barraR = 0, barraC = 0, barra = 0, aux = 0;
+let barraD = 0, barraP = 0, barraR = 0, barraC = 0, barra = 0, aux = 0;
 
 $("#New").on('click', function(){
 	$('#Nova').toggle();
+	$("#Title").val("");
+	$("#Description").val("");
+	barra++;
+	calcularBARRA();
 
 });
 $("#exit").on('click', function(){
 	$('#Nova').toggle();
+	barra--;
+	calcularBARRA();
+
 })
 $("#Sub").on('click', function(){
 	
@@ -13,55 +20,62 @@ $("#Sub").on('click', function(){
 	new_div();
 })
 
+$(document).on("click", "#ConteudoTD .tarefa" ,function(){
+ 
+		barraP++;
+		barraD--;
+		console.log(barraD);
+		calcularBARRA();
+		$(this).remove();
+		$("#ConteudoIP").append($(this));
+})
+
+$(document).on("click", "#ConteudoIP .tarefa" ,function(){
+		barraP--;
+		barraR++;
+		calcularBARRA();
+		$(this).remove();
+		$("#ConteudoRV").append($(this));
+})
+
+$(document).on("click", "#ConteudoRV .tarefa" ,function(){
+		barraR--;
+		barraC++;
+		calcularBARRA();
+		$(this).remove();
+		$("#ConteudoCP").append($(this));
+})
+
 function new_div(){
 $(document).ready(function(){
-	barra++;barraT++;
 	calcularBARRA();
 	var titleValues = $("#Title").val();
 	var descriValues = $("#Description").val();
+
 	var descric={
 		id: "descrc",
 		classe: "descrc",
 	};
 	var tituloc ={
-		classe: "xablau",
-		id:"titulo1",
 		
-
+		id:"titulo1",
 	};
-
 	var $descricao = $("<div>", descric);
 	$descricao.html(descriValues)
-
 	var $titulo = $("<div>", tituloc);
 	$titulo.html(titleValues);
-
-	$("#ConteudoTD").append($titulo); 	
-	barraT--;
-	$("#titulo1").on('click', function(){
-	$("#ConteudoIP").append($titulo);
-	barraP++;
+	$titulo.append($descricao);
+	$titulo.attr("id", titleValues);
+	$titulo.attr("class", "tarefa");
+	$("#ConteudoTD").append($titulo); 
+	barraD++;console.log(barraD);
 	calcularBARRA();	
-	barraP--;
-	$("#titulo1").on('click', function(){
-	$("#ConteudoRV").append($titulo);
-	barraR++;
-	calcularBARRA();	
-	barraR--;
-	$("#titulo1").on('click', function(){
-	barraC++;
-	calcularBARRA();	
-	$("#ConteudoCP").append($titulo);
-	
 })	
-})
-})
-})
 }
 
 function calcularBARRA(){
-	let tamanho = (barraT * 100)/barra;
-	$('#BToDo').css("width",`${tamanho}%`);
+	let tamanho0 = (barraD * 100)/barra;
+	$('#BToDo').css("width",`${tamanho0}%`);
 	let tamanho1 = (barraP * 100)/barra;
 	$('#BInPro').css("width",`${tamanho1}%`);
 	let tamanho2 = (barraR * 100)/barra;
@@ -69,3 +83,4 @@ function calcularBARRA(){
 	let tamanho3 = (barraC * 100)/barra;
 	$('#BComp').css("width",`${tamanho3}%`);
 };
+
